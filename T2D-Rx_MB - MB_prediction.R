32803cff38728bfbd07671fb7a71079c802d644c
k-annabel <annabel.klemets@protonmail.com>
2023-12-12 12:17:55 +0200

    Add files via upload

---
+++ T2D-Rx_MB - MB_prediction.R
@@ -0,0 +1,78 @@
+# T2D-Rx_MB_analysis - MB prediction ability of T2D treatment endpoints
+
+# Load packages
+library(here)
+library(dplyr)
+library(flextable)
+library(tidyverse)
+library(tibble)
+library(stringr)
+library(ggplot2)
+library(mia)
+library(miaViz)
+library(purrr)
+library(tidyr)
+library(vegan)
+library(pheatmap)
+library(ggcorrplot)
+library(ggpubr)
+library(paletteer)
+library(ggthemes)
+library(rstatix)
+
+# ___________________________________________________________________________ #
+
+# Load input files
+
+## Load counts (otu-matrix)
+counts <- read.csv("otu_matrix.csv", sep = ";", row.names = 1, check.names = FALSE)
+
+## Load taxonomy (rowData)
+tax <- read.csv("taxonomy.csv", sep = ";", row.names = 1, check.names = FALSE)
+
+## Load metadata (colData)
+samples <- read.csv("metadata.csv", row.names = 1, sep = ",", check.names = FALSE)
+
+## Order columns in counts based on rows in samples
+counts <- counts[ , rownames(samples)]
+
+## Convert input files into right format
+counts <- as.matrix(counts)
+
+## Make a TreeSummarizedExperiment
+tse_taxa <- TreeSummarizedExperiment(assays =  SimpleList(counts = counts),
+                                     colData = DataFrame(samples),
+                                     rowData = DataFrame(tax))
+
+## Remove features containing human DNA
+tse_prelim <- tse_taxa[!rowData(tse_taxa)$Phylum == "" & 
+                         !rowData(tse_taxa)$Class == "" & 
+                         !rowData(tse_taxa)$Kingdom == "Unassigned", ]
+
+## Exclude samples
+tse <- tse_prelim[ , !colnames(tse_prelim) %in% c("GLP1RA-5-2", "GLP1RA-5-3", 
+                                                  "GLP1RA-5-4", "GLP1RA-6-2", 
+                                                  "NEG-Valida", "GLP1RA-15-2-2", 
+                                                  "Elini-proov", "AL")]
+
+
+# ___________________________________________________________________________ #
+
+
+# Convert counts into relative abundances
+tse <- transformCounts(tse, assay.type = "counts", method = "relabundance")
+
+# Convert relative abundances into CLR-transformed values
+mat <- assay(tse, "relabundance")
+tse <- transformCounts(tse, assay.type = "relabundance", method = "clr", 
+                       pseudocount = min(mat[mat>0]))
+
+# Collapse into Genus level
+tse_genus <- agglomerateByRank(tse, rank = "Genus")
+
+# Remove "Genus:" label
+rownames(tse_genus) <- sub("Genus:", "", rownames(tse_genus))
+
+# Separate by medication
+tse_glp <- tse_genus[ , colData(tse_genus)$Medication == "GLP-1-RA"]
+tse_sglt <- tse_genus[ , colData(tse_genus)$Medication == "SGLT-2"]
